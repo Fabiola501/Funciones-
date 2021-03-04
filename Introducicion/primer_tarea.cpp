f602b92c7116066d8cce65b04f829c2df1758bce
Fabiola501 <79536789+Fabiola501@users.noreply.github.com>
2021-03-03 20:25:50 -0600

    Add files via upload

---
+++ Introducicion/primer_tarea.cpp
@@ -0,0 +1,19 @@
+//Sanchez Perez Fabiola 2AV4
+#include <iostream>
+
+int main()
+{
+
+	int NumeroA;
+	int NumeroB;
+	int Suma;
+
+	std::cout<<"Introduzca el primer operando:\n"; std::cin>>NumeroA;
+	std::cout<<"Introduzca el segundo operando:\n"; std::cin>>NumeroB;
+
+
+	Suma=NumeroA+NumeroB;
+	std::cout<<"El resultado de la suma es:"<<Suma<<::std::endl;
+
+	return 0;
+}
